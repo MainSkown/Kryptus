bd30b26609f348af2c50c913a801c4da3102d415
MainSkown <mainskown.contact@gmail.com>
2024-04-06 17:41:48 +0200

    Created user menu

--- app.py
+++ app.py
@@ -12,7 +12,75 @@ def main():
     # Read config
     config = read_config()
 
-    print(decrypt(encrypt('Hello World', config['key']), config['key']))
+    # User menu
+    # 1. Encrypt message
+    # 1.1 Use config
+    # 1.2 User values
+    # 2. Decrypt message
+    # 2.1 Use config
+    # 2.2 User values
+    # 3. Exit
+
+    while True:
+        print('1. Encrypt message')
+        print('2. Decrypt message')
+        print('3. Exit')
+        choice = input('Enter your choice: ')
+
+        match choice:
+            case '1':
+                print('1. Use config')
+                print('2. User values')
+                choice = input('Enter your choice: ')
+
+                text = ""
+                key = ""
+                save_location = ""
+
+                match choice:
+                    case '1':
+                        text = open(config['file-location'], 'r').read()
+                        save_location = config['encrypted-file-location']
+                        key = config['key']
+                    case '2':
+                        text = open(input('Enter file location: '), 'r').read()
+                        save_location = input('Enter save location: ')
+                        key = input('Enter key: ')
+
+                # Encrypt text
+                encrypted_text = encrypt(text, key)
+                # Save encrypted text
+                f = open(save_location, 'w')
+                f.write(encrypted_text)
+                f.close()
+            case '2':
+                print('1. Use config')
+                print('2. User values')
+                choice = input('Enter your choice: ')
+
+                text = ""
+                key = ""
+                save_location = ""
+
+                match choice:
+                    case '1':
+                        text = open(config['encrypted-file-location'], 'r').read()
+                        save_location = config['decrypted-file-location']
+                        key = config['key']
+                    case '2':
+                        text = open(input('Enter file location: '), 'r').read()
+                        save_location = input('Enter save location: ')
+                        key = input('Enter key: ')
+
+                # Decrypt text
+                decrypted_text = decrypt(text, key)
+                # Save decrypted text
+                f = open(save_location, 'w')
+                f.write(decrypted_text)
+                f.close()
+            case '3':
+                break
+            case _: print('Invalid choice')
 
 
 if __name__ == '__main__':
